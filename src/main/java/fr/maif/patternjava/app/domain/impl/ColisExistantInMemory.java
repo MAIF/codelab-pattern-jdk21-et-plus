3fd0735fc14d6eb753472b85d49f2ccfab787bdb
Alexandre Delègue <alexandre.delegue@maif.fr>
2024-09-12 16:56:53 +0200

    Etape 3

--- src/main/java/fr/maif/patternjava/app/domain/impl/ColisExistantInMemory.java
+++ src/main/java/fr/maif/patternjava/app/domain/impl/ColisExistantInMemory.java
@@ -10,25 +10,25 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class ColisExistantInMemory implements ColisExistants {
 
-    private final ConcurrentHashMap<String, Colis> store = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Colis.ColisExistant> store = new ConcurrentHashMap<>();
 
     @Override
-    public Colis chercherColisExistantParReference(String referenceColis) {
+    public Colis.ColisExistant chercherColisExistantParReference(String referenceColis) {
         return store.get(referenceColis);
     }
 
     @Override
-    public Colis enregistrerColis(Colis colisPrisEnCharge) {
+    public Colis.ColisExistant enregistrerColis(Colis.ColisExistant colisPrisEnCharge) {
         return store.compute(colisPrisEnCharge.reference(), (id, colisExistant) -> colisPrisEnCharge);
     }
 
     @Override
-    public Colis mettreAJourColis(Colis colis) {
+    public Colis.ColisExistant mettreAJourColis(Colis.ColisExistant colis) {
         return store.compute(colis.reference(), (id, colisExistant) -> colis);
     }
 
     @Override
-    public List<Colis> listerColis() {
+    public List<Colis.ColisExistant> listerColis() {
         return store.values().stream().toList();
     }
 }
