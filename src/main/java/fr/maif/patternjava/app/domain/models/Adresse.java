ccd3dd7a09b2171f421ed4aead6644d4fcb5d85d
Alexandre Delègue <alexandre.delegue@maif.fr>
2024-09-12 15:33:26 +0200

    Etape 2

--- src/main/java/fr/maif/patternjava/app/domain/models/Adresse.java
+++ src/main/java/fr/maif/patternjava/app/domain/models/Adresse.java
@@ -1,52 +1,45 @@
 package fr.maif.patternjava.app.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import java.util.Objects;
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        property = "type"
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(
+                value = Adresse.AdresseBtoB.class,
+                name = "AdresseBtoB"
+        ),
+        @JsonSubTypes.Type(
+                value = Adresse.AdresseBtoC.class,
+                name = "AdresseBtoC"
+        )}
+)
+public sealed interface Adresse {
+        @Builder(toBuilder = true)
+        record AdresseBtoB(
+                @NotNull @Length(max = 38) String raisonSocialeOuDenomination,
+                @NotNull @Length(max = 38) String identiteDestinataireOuService,
+                @Length(max = 38) String mentionSpecialeEtCommuneGeo,
+                @Length(max = 38) String entreeBatimentImmeubleResidence,
+                @NotNull @Length(max = 38) String numeroLibelleVoie,
+                @NotNull @Length(max = 38) String codePostalEtLocaliteOuCedex,
+                @Length(max = 38) String pays
+        ) implements Adresse { }
 
-@Data
-@Builder(toBuilder = true)
-public class Adresse {
-        @NotNull
-        public TypeAdresse type;
-        // Commun
-        @Length(max = 38) String entreeBatimentImmeubleResidence;
-        @NotNull @Length(max = 38) String numeroLibelleVoie;
-        @NotNull @Length(max = 38) String codePostalEtLocaliteOuCedex;
-        @Length(max = 38) String pays;
-
-        // B2B
-        @Length(max = 38) String raisonSocialeOuDenomination;
-        @Length(max = 38) String identiteDestinataireOuService;
-        @Length(max = 38) String mentionSpecialeEtCommuneGeo;
-
-        // B2C
-        @Length(max = 38) String civiliteNomPrenom;
-        @Length(max = 38) String noAppEtageCouloirEscalier;
-        @Length(max = 38) String lieuDitServiceParticulierDeDistribution;
-
-        public Adresse(TypeAdresse type, String entreeBatimentImmeubleResidence, String numeroLibelleVoie, String codePostalEtLocaliteOuCedex, String pays, String raisonSocialeOuDenomination, String identiteDestinataireOuService, String mentionSpecialeEtCommuneGeo, String civiliteNomPrenom, String noAppEtageCouloirEscalier, String lieuDitServiceParticulierDeDistribution) {
-                this.type = type;
-                this.entreeBatimentImmeubleResidence = entreeBatimentImmeubleResidence;
-                this.numeroLibelleVoie = numeroLibelleVoie;
-                this.codePostalEtLocaliteOuCedex = codePostalEtLocaliteOuCedex;
-                this.pays = pays;
-
-                if (type == TypeAdresse.AdresseBtoB) {
-                        Objects.requireNonNull(raisonSocialeOuDenomination, "required pour une adresse B2B");
-                        Objects.requireNonNull(identiteDestinataireOuService, "required pour une adresse B2B");
-                } else {
-                        Objects.requireNonNull(civiliteNomPrenom, "required pour une adresse B2C");
-                }
-                this.raisonSocialeOuDenomination = raisonSocialeOuDenomination;
-                this.identiteDestinataireOuService = identiteDestinataireOuService;
-                this.mentionSpecialeEtCommuneGeo = mentionSpecialeEtCommuneGeo;
-
-                this.civiliteNomPrenom = civiliteNomPrenom;
-                this.noAppEtageCouloirEscalier = noAppEtageCouloirEscalier;
-                this.lieuDitServiceParticulierDeDistribution = lieuDitServiceParticulierDeDistribution;
-        }
+        @Builder(toBuilder = true)
+        record AdresseBtoC(
+                @NotNull @Length(max = 38) String civiliteNomPrenom,
+                @Length(max = 38) String noAppEtageCouloirEscalier,
+                @Length(max = 38) String lieuDitServiceParticulierDeDistribution,
+                @Length(max = 38) String entreeBatimentImmeubleResidence,
+                @NotNull @Length(max = 38) String numeroLibelleVoie,
+                @NotNull @Length(max = 38) String codePostalEtLocaliteOuCedex,
+                @Length(max = 38) String pays
+        ) implements Adresse { }
 }
