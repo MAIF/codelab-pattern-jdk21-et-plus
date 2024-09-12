102579caafda810031a4a5e7dc4bc04f180bf537
Alexandre Delègue <alexandre.delegue@maif.fr>
2024-09-12 16:56:53 +0200

    Etape 3

--- src/main/java/fr/maif/patternjava/app/domain/models/Colis.java
+++ src/main/java/fr/maif/patternjava/app/domain/models/Colis.java
@@ -3,25 +3,43 @@ package fr.maif.patternjava.app.domain.models;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
+import lombok.Data;
 
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@Builder(toBuilder = true)
-public record Colis(
-        String reference,
-        @NotNull TypeColis type,
-        @NotNull LocalDateTime dateDEnvoi,
-        LocalDateTime dateDeReception,
-        Double latitude,
-        Double longitude,
-        @Email @NotNull String email,
-        @Valid Adresse adresse) {
+public sealed interface Colis {
 
-    public Colis {
-        dateDEnvoi = Objects.requireNonNullElse(dateDEnvoi, LocalDateTime.now());
+    sealed interface ColisExistant extends Colis {
+        String reference();
+    }
+
+    @Builder
+    record NouveauColis(LocalDateTime dateDEnvoi, @NotNull @Email String email, @NotNull @Valid Adresse adresse) implements Colis {
+
+        public NouveauColis {
+            dateDEnvoi = Objects.requireNonNullElse(dateDEnvoi, LocalDateTime.now());
+        }
+
+        public ColisPrisEnCharge toColisPrisEnCharge(String reference) {
+            return new ColisPrisEnCharge(reference, dateDEnvoi, email, adresse);
+        }
+    }
+
+    record ColisPrisEnCharge(@NotNull String reference, @NotNull LocalDateTime dateDEnvoi, @NotNull @Email String email, @NotNull @Valid Adresse adresse) implements ColisExistant {
+
+    }
+
+    record ColisEnCoursDAcheminement(@NotNull String reference, @NotNull LocalDateTime dateDEnvoi, @NotNull Double latitude, @NotNull Double longitude, @NotNull @Email String email, @NotNull @Valid Adresse adresse) implements ColisExistant {
+
+    }
+
+    @Builder
+    record ColisRecu(@NotNull String reference, @NotNull LocalDateTime dateDEnvoi, @NotNull LocalDateTime dateDeReception, @NotNull @Email String email,
+                     @NotNull @Valid Adresse adresse) implements ColisExistant {
+
     }
 }
 
