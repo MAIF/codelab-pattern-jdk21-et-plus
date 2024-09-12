37440cfd793c2f0a062157cf895dc2c152644e2a
Alexandre Delègue <alexandre.delegue@maif.fr>
2024-09-12 15:17:04 +0200

    Etape 1

--- src/main/java/fr/maif/patternjava/app/domain/LivraisonDeColis.java
+++ src/main/java/fr/maif/patternjava/app/domain/LivraisonDeColis.java
@@ -28,7 +28,7 @@ public class LivraisonDeColis {
 
 
     public Colis prendreEnChargeLeColis(Colis colis) throws EtatInvalide {
-        if (colis.type.equals(TypeColis.NouveauColis)) {
+        if (colis.type().equals(TypeColis.NouveauColis)) {
             var reference = genererReference();
             return this.colisExistants.enregistrerColis(colis.toBuilder()
                     .reference(reference)
@@ -40,32 +40,32 @@ public class LivraisonDeColis {
     }
 
     public Colis gererColis(Colis colis) {
-        if (colis.type.equals(TypeColis.NouveauColis)) {
+        if (colis.type().equals(TypeColis.NouveauColis)) {
             throw new EtatInvalide("Le colis ne doit pas être un nouveau colis");
         } else {
-            var colisExistant = this.colisExistants.chercherColisExistantParReference(colis.reference);
+            var colisExistant = this.colisExistants.chercherColisExistantParReference(colis.reference());
             if (Objects.isNull(colisExistant)) {
-                throw new ColisNonTrouve(colis.reference);
+                throw new ColisNonTrouve(colis.reference());
             }
 
-            if ((colisExistant.type.equals(ColisPrisEnCharge) && colis.type.equals(ColisEnCoursDAcheminement)) ||
-                (colisExistant.type.equals(ColisEnCoursDAcheminement) && colis.type.equals(ColisEnCoursDAcheminement)) ||
-                (colisExistant.type.equals(ColisEnCoursDAcheminement) && colis.type.equals(ColisRecu))) {
+            if ((colisExistant.type().equals(ColisPrisEnCharge) && colis.type().equals(ColisEnCoursDAcheminement)) ||
+                (colisExistant.type().equals(ColisEnCoursDAcheminement) && colis.type().equals(ColisEnCoursDAcheminement)) ||
+                (colisExistant.type().equals(ColisEnCoursDAcheminement) && colis.type().equals(ColisRecu))) {
 
-                if (colisExistant.type.equals(ColisPrisEnCharge) &&
-                    colisExistant.dateDEnvoi.isBefore(LocalDateTime.now().minusMonths(1))) {
+                if (colisExistant.type().equals(ColisPrisEnCharge) &&
+                    colisExistant.dateDEnvoi().isBefore(LocalDateTime.now().minusMonths(1))) {
                     throw new EtatInvalide("La prise en charge date de plus d'1 mois");
                 }
 
                 return this.colisExistants.mettreAJourColis(colis);
             }
-            if (colisExistant.type.equals(ColisPrisEnCharge)) {
+            if (colisExistant.type().equals(ColisPrisEnCharge)) {
                 throw new EtatInvalide("On attend un colis à l'état \"ColisEnCoursDAcheminement\"");
             }
-            if (colisExistant.type.equals(ColisEnCoursDAcheminement)) {
+            if (colisExistant.type().equals(ColisEnCoursDAcheminement)) {
                 throw new EtatInvalide("On attend un colis à l'état \"ColisEnCoursDAcheminement\" ou \"ColisPrisEnCharge\"");
             }
-            if (colisExistant.type.equals(ColisRecu)) {
+            if (colisExistant.type().equals(ColisRecu)) {
                 throw new EtatInvalide("Le colis est déjà reçu");
             }
             throw new EtatInvalide("Cas non géré");
