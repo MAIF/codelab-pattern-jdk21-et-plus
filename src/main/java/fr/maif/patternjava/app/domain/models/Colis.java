cb9a794b91cd07a1827c23900431afc3b1f852c9
Alexandre Delègue <alexandre.delegue@maif.fr>
2024-09-12 15:17:04 +0200

    Etape 1

--- src/main/java/fr/maif/patternjava/app/domain/models/Colis.java
+++ src/main/java/fr/maif/patternjava/app/domain/models/Colis.java
@@ -3,39 +3,27 @@ package fr.maif.patternjava.app.domain.models;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
-import lombok.Data;
 
 import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
- @Data
- @Builder(toBuilder = true)
- public class Colis {
-     public String reference;
-     @NotNull
-     public TypeColis type;
-     @NotNull
-     public LocalDateTime dateDEnvoi;
-     public LocalDateTime dateDeReception;
-     public Double latitude;
-     public Double longitude;
-     @Email
-     @NotNull
-     public String email;
-     public @Valid Adresse adresse;
+@Builder(toBuilder = true)
+public record Colis(
+        String reference,
+        @NotNull TypeColis type,
+        @NotNull LocalDateTime dateDEnvoi,
+        LocalDateTime dateDeReception,
+        Double latitude,
+        Double longitude,
+        @Email @NotNull String email,
+        @Valid Adresse adresse) {
 
-     public Colis(String reference, TypeColis type, LocalDateTime dateDEnvoi, LocalDateTime dateDeReception, Double latitude, Double longitude, String email, Adresse adresse) {
-         this.reference = reference;
-         this.type = type;
-         this.dateDEnvoi = Objects.requireNonNullElse(dateDEnvoi, LocalDateTime.now());
-         this.dateDeReception = dateDeReception;
-         this.latitude = latitude;
-         this.longitude = longitude;
-         this.email = email;
-         this.adresse = adresse;
-     }
- }
+    public Colis {
+        dateDEnvoi = Objects.requireNonNullElse(dateDEnvoi, LocalDateTime.now());
+    }
+}
 
 
 
